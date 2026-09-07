fbcfa6f29c6839bb0189a658a8ae960577b8c3e0
RafifMuh <rafifmoh09@gmail.com>
2026-09-07 23:14:15 +0700

    Force Vercel safe environment variables in entrypoint

--- api/index.php
+++ api/index.php
@@ -1,4 +1,17 @@
 <?php
 
+// Force Vercel-compatible environment variables (overrides user dashboard settings if they conflict)
+putenv('LOG_CHANNEL=stderr');
+$_ENV['LOG_CHANNEL'] = 'stderr';
+
+putenv('SESSION_DRIVER=cookie');
+$_ENV['SESSION_DRIVER'] = 'cookie';
+
+putenv('CACHE_DRIVER=array');
+$_ENV['CACHE_DRIVER'] = 'array';
+
+putenv('VIEW_COMPILED_PATH=/tmp');
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp';
+
 // Forward requests to the Laravel public index.php file
 require __DIR__ . '/../public/index.php';
